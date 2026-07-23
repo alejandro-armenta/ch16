c0dfa2e28c58d773078c82607216c07944645eb8
Alejandro <armentaayalaalejandro@gmail.com>
2026-07-23 17:36:47 -0600

    added navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import PropTypes from 'prop-types'
+
+export function UserBar({ username }) {
+  return (
+    <form>
+      <Link href='/create'>Create Post</Link> | Logged in as{' '}
+      <strong>{username}</strong> <button>Log out</button>
+    </form>
+  )
+}
+
+UserBar.propTypes = { username: PropTypes.string.isRequired }
+
+export function LoginSignupLinks() {
+  return (
+    <div>
+      <Link href='/login'>Log In</Link> | <Link href='/signup'>Sign Up</Link>
+    </div>
+  )
+}
+
+export function Navigation({ username }) {
+  return (
+    <>
+      <Link href='/'>Home</Link>
+      {username ? <UserBar username={username} /> : <LoginSignupLinks />}
+    </>
+  )
+}
+
+Navigation.propTypes = { username: PropTypes.string }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import PropTypes from 'prop-types'
 
 export function Post({ _id, title, author }) {
   return (
     <article>
-      <h3>{title}</h3>
+      <h3>
+        <Link href={`/posts/${_id}`}>{title}</Link>
+      </h3>
       <em>
         Written by <strong>{author.username}</strong>
       </em>
